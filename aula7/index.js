7ea77051e516bf65575d4941c543e1850ca3f0cb
4lineferreira <alineferreirasnsd@gmail.com>
2024-08-24 16:28:41 -0300

    Initial commit

--- aula7/index.js
+++ aula7/index.js
@@ -1,3 +1,5 @@
+/*
+
 const primeiroNumero = 5;
 const segundoNumero = 10;
 const resultado = primeiroNumero * segundoNumero;
@@ -13,6 +15,8 @@ console.log(typeof segundoNumero);
 
 
 console.log(typeof primeiroNumero + segundoNumero);
+*/
+
 
 // exemplo de alturaEmM
 
@@ -20,7 +24,16 @@ const nome = 'Aline';
 const sobrenome = 'Ferreira';
 const idade = 25;
 const peso = 78;
-const alturaEmCm = 1.69; 
+const alturaEmM = 1.69; 
+let imc = peso / (alturaEmM * alturaEmM )
+const anoDeNascimento = 1999
 
-console.log('Eu me chamo', nome, sobrenome, 'tenho', idade, 'anos e peso', peso, 'minha altura é', alturaEmCm, 'e estou aprendendo a codar')
+console.log('Eu me chamo', nome, sobrenome, 'tenho', idade, 'anos e peso', peso, 'Kg e a minha altura é', alturaEmM, 'e estou aprendendo a codar')
+console.log('meu IMC é de',imc )
+console.log(nome, sobrenome, 'nascida em', anoDeNascimento, 'está estudando programação para fazer a certificação de AWS' )
 
+//não consigo usar craze pq meu teclado é 60% tive que copiar e colcar o exemplo abaixo
+
+console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso} kg`);
+console.log(`tem ${alturaEmM} de altura e seu IMC é de ${imc}`);
+console.log(`${nome} nasceu em ${anoDeNascimento}.`);
---
+++ aula9/index.js
@@ -0,0 +1,18 @@
+const nome ='aline'; //string
+const nome1 ="aline"; //string
+const nome2 =`aline`; //string
+const num1 = 10; //number
+const num2 =10.53 //number
+let nomeAluno; //undefineds -> não aponta pra local nenhum na memória
+let sobrenomeAluno = null; //nulo -> não aponta pra local nenhum na memória
+const aprovado = true; //Valor boolean = true ou false (lógico) desvio condicional
+ 
+console.log(typeof nomeAluno, num2);
+
+//Existe alguns dados primitivos sendo eles String, number, undefined, null, boolean e symbol(symbol é mais avançado será mencionado mais pra frente
+
+let a = 123;
+const b = a
+
+console.log(a, b);
+
